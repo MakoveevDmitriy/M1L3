59e13a15541612799c75cb7b65caaf310abf0d29
MakoveevDmitriy <131581608+MakoveevDmitriy@users.noreply.github.com>
2023-05-07 13:59:47 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import discord
+from bot_logic import gen_pass
+import random
+
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    elif message.content.startswith('password'):
+        gen_pass(10)
+    elif message.content.startswith('подбрось монетку'):
+        a = random.randint('орёл', 'решка')
+        print(a)
+    
+    elif message.content.startswith('смайлик'):
+        def gen_emodji():
+            emoji = [':)','=)','о_О',':-<','*О*']
+            return random.choice(emoji)
+    else:
+        await message.channel.send(message.content)
+client.run("MTEwNDY3NDAyMjc5NDQ2NTM4MA.G-lX4n.TnDYOirN3YhsPDtVJDxQZBHvNcUn-mkqEi6Fx0")
