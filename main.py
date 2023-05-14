4dcec5c31a77231c74f4bf5a44776ee05a573d0c
MakoveevDmitriy <131581608+MakoveevDmitriy@users.noreply.github.com>
2023-05-14 13:22:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
+import random
 from discord.ext import commands
+from bot_logic import gen_pass
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -18,7 +20,18 @@ async def hello(ctx):
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
-
+@bot.command()
+async def passvord(ctx):
+    
+    await ctx.send(gen_pass(10))
+    
+@bot.command()
+async def money(ctx):
+    a = random.randint(1, 2)
+    if a == 1:
+        await ctx.send('решка')
+    else:
+        await ctx.send('орёл')
 
 
 bot.run("токен")
